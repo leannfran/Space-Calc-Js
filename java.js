0d1ed11b28b4daee94619fd6945943dfb7458469
= <leandrofranco50@yahoo.com.ar>
2022-08-25 19:57:27 -0300

    second commit

--- java.js
+++ java.js
@@ -1,47 +1,46 @@
+//hola pablo como veras estoy retrasado por que me estan matando en la facu lo entrego pra no perder la entrega pero me voy a poner a hacerlo ahora despues de rendir mildisculpas
 //LEANDRO FRANCO
-
+//igual estuve tocando cosas je
+//la entrega pasada no me figura como entregada nose que paso
    //alert("Bienvenido, esta es una calculadora espacial")
-   let nombre = document.getElementById("ingrese__nombre")
-   let peso = document.getElementById("ingrese__peso")
-   let planeta = document.getElementById("ingrese__planeta")
-   let resultado;
-   let nombrePlaneta;
-   let gTierra = 9.8;
-   let gMarte = 3.7;
-   let gJupiter = 24.8;
-   let pesoFinal;
-   let escritura = ("el peso de " + nombre + " en " + nombrePlaneta + " es de " + pesoFinal + "kg")
+   function funBoton() {
+
+    let nombre = document.getElementById("ingrese__nombre").value;
+  
+    let peso = document.getElementById("ingrese__peso").value;
+  
+    let planeta = document.getElementById("ingrese__planeta").value;
+    let pesoFinal;
+    let gTierra = 9.8;
+    let gMarte = 3.7;
+    let gJupiter = 24.8;
+
+    function cuentaMarte () {
+        return peso * gMarte / gTierra;
+        }
     
-function fun1 () {
-    nombre = ing
-}
+    function cuentaaJupiter () {
+        return peso * gJupiter / gTierra;
+        }
 
-   function funBoton () {
-    pesoFinal__h3.innerHTML = escritura;
-   }
-
-    boton__calc.addEventListener('click', funBoton)
-
-   
-function cuentaMarte () {
-    return peso * gMarte / gTierra;
-    }
-
-function cuentaaJupiter () {
-    return peso * gJupiter / gTierra;
-    }
-
-    if (planeta == 1) {
+    if (planeta == "marte") {
         pesoFinal = cuentaMarte();
-        nombrePlaneta = "Marte";
-        
-    } else if (planeta == 2) {
-        pesoFinal = cuentaaJupiter();
-        nombrePlaneta = "Jupiter";
-    }
-    
+        }
+    else if (planeta == "Marte") {
+        pesoFinal = cuentaMarte();
+        }
+    else if (planeta == "jupiter") {
+            pesoFinal = cuentaaJupiter();
+        }
+    else if (planeta == "Jupiter") {
+                pesoFinal = cuentaaJupiter();
+        }
+
     pesoFinal = parseInt(pesoFinal)
 
+    let escritura = "el peso de " + nombre + " en " + planeta + " es de " + pesoFinal + "kg";
+  
+    pesoFinal__h3.innerHTML = escritura;
 
     const guardarLocal = (clave, valor) => {
         localStorage.setItem(clave, valor);
@@ -54,18 +53,29 @@ function cuentaaJupiter () {
     let pesoAdecuado = ("usted es Saludable")
     peso > 100 ? document.getElementById("container__sobrePeso").innerHTML = sobrePeso : document.getElementById("container__sobrePeso").innerHTML = pesoAdecuado
     
-class persona {
-    constructor (nombre, peso, nombrePlaneta, pesoFinal) {
-        this.nombre = nombre;
-        this.peso = peso;
-        this.nombrePlaneta = nombrePlaneta;
-        this.pesoFinal = pesoFinal;
+    class persona {
+        constructor (nombre, peso, planeta, pesoFinal) {
+            this.nombre = nombre;
+            this.peso = peso;
+            this.planeta = planeta;
+            this.pesoFinal = pesoFinal;
+            }
         }
+    const personas = new persona (nombre, peso, planeta, pesoFinal);
+        console.log(personas);
+  
+  }
+  
+  boton__calc.addEventListener("click", funBoton);
+
+  boton__calc.addEventListener("click", () =>{
+    swal("tu resultado esta listo!", "", "success");
+  })
+  
+  function cuentaMarte () {
+      return peso * gMarte / gTierra;
     }
-const personas = new persona (nombre, peso, nombrePlaneta, pesoFinal);
-    console.log(personas);
-
-
+    
 let container = document.getElementById("container__otrasPersonas")
     console.log(container);
 let valores = [
@@ -81,8 +91,3 @@ for (const valor of valores) {
                         
     container.appendChild(parram)
 }
-
-
-//asi si me anda boton.addEventListener("click", respuestaCl);
-//function respuestaCl() {
-  //  swal("te dije que no tocaras el boton!", "🤬");
